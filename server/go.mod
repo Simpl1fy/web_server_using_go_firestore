ff4b0ba0d67a5839f6d0a48c63a905add6f8897d
Simpl1fy <gourabdas07012004@gmail.com>
2024-10-16 13:30:06 +0530

    created a go server and serving in port 5000 and created a end point for /

---
+++ server/go.mod
@@ -0,0 +1,3 @@
+module web_server/learn
+
+go 1.23.1
--- server/main.go
+++ server/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, _ *http.Request) {
+	_, err := io.WriteString(w, "Welcome to the basic Http server")
+	if err != nil {
+		fmt.Println("error", err)
+		return
+	}
+}
 
 func main() {
 	fmt.Println("Hello World this is a web server in go")
+
+	http.HandleFunc("/", handler)
+	fmt.Println("Listening to port 5000")
+	err := http.ListenAndServe(":5000", nil)
+	if err != nil {
+		fmt.Println("Error while listening and serving =", err)
+		return
+	}
 }
